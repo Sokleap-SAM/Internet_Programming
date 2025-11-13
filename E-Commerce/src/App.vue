e4ed888527445c36d94b49f596295af5ee27c905
Sokleap-SAM <leapngu2019@gmail.com>
2025-11-13 16:40:46 +0700

    practice03

---
+++ E-Commerce/src/App.vue
@@ -0,0 +1,79 @@
+<script>
+import CategoryComponent from './components/CategoryComponent.vue'
+import PromotionComponent from './components/PromotionComponent.vue'
+const burgerImage = new URL('@/assets/burger.png', import.meta.url).href
+const peachImage = new URL('@/assets/peach.png', import.meta.url).href
+const kiwiImage = new URL('@/assets/kiwi.png', import.meta.url).href
+const appleImage = new URL('@/assets/apple.png', import.meta.url).href
+const snackImage = new URL('@/assets/snack.png', import.meta.url).href
+const plumImage = new URL('@/assets/plum.png', import.meta.url).href
+const cabbageImage = new URL('@/assets/cabbage.png', import.meta.url).href
+const headphoneImage = new URL('@/assets/headphone.png', import.meta.url).href
+const cakeImage = new URL('@/assets/cake.png', import.meta.url).href
+const orangeImage = new URL('@/assets/orange.png', import.meta.url).href
+const onionImage = new URL('@/assets/onion.jpg', import.meta.url).href
+const juiceImage = new URL('@/assets/juice.png', import.meta.url).href
+const backetImage = new URL('@/assets/backet.jpg', import.meta.url).href
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      burgerImage,
+      peachImage,
+      kiwiImage,
+      appleImage,
+      snackImage,
+      plumImage,
+      cabbageImage,
+      headphoneImage,
+      cakeImage,
+      orangeImage,
+      onionImage,
+      juiceImage,
+      backetImage,
+    }
+  },
+  components: {
+    CategoryComponent,
+    PromotionComponent,
+  },
+}
+</script>
+
+<template>
+  <div class="app">
+    <div class="category-wrapper">
+    <CategoryComponent title="Cake & Milk" :product-count="14" :image="burgerImage" container-color="#F2FCE4" />
+    <CategoryComponent title="Peach" :product-count="17" :image="peachImage" container-color="#FFFCEB" />
+    <CategoryComponent title="Organic Wiki" :product-count="21" :image="kiwiImage" container-color="#ECFFEC" />
+    <CategoryComponent title="Apple" :product-count="68" :image="appleImage" container-color="#FEEFEA" />
+    <CategoryComponent title="Snack" :product-count="34" :image="snackImage" container-color="#FFF3EB" />
+    <CategoryComponent title="Plum" :product-count="25" :image="plumImage" container-color="#FFF3FF" />
+    <CategoryComponent title="Cabbage" :product-count="65" :image="cabbageImage" container-color="#F2FCE4" />
+    <CategoryComponent title="Headphone" :product-count="33" :image="headphoneImage" container-color="#FFFCEB" />
+    <CategoryComponent title="Cake" :product-count="53" :image="cakeImage" container-color="#F2FCE4" />
+    <CategoryComponent title="Orange" :product-count="63" :image="orangeImage" container-color="#FFF3FF" />
+  </div>
+  <div class="promotion-wrapper">
+    <PromotionComponent title="Everyday Fresh & Clean with Our Products" :image="onionImage" container-color="#F0E8D5" />
+    <PromotionComponent title="Make your Breakfast Healthy and Easy" :image="juiceImage" container-color="#F3E8E8" />
+    <PromotionComponent title="The best Organic Products Online" :image="backetImage" container-color="#E7EAF3" button-color = "#FDC040" />
+  </div>
+  </div>
+</template>
+
+<style scoped>
+.category-wrapper {
+  display: flex;
+  flex-direction: row;
+  gap: 24px;
+  justify-content: flex-start;
+}
+.promotion-wrapper {
+  margin-top: 70px;
+  display: flex;
+  flex-direction: row;
+  gap: 24px;
+}
+</style>
---
+++ E-Commerce/src/components/CategoryComponent.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+    <img :src="image" />
+    <h3 class="title">{{ title }}</h3>
+    <span class="count">{{ productCount }} items</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CategoryComponent',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    productCount: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      required: true,
+    },
+    containerColor: {
+      type: String,
+      default: '#81b13d99',
+    },
+  },
+}
+</script>
+
+<style scoped>
+.container {
+  border: 1px solid #81b13d;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0px 9px 10px 8.5px;
+  background-color: v-bind('containerColor');
+}
+.count {
+  color: #B6B6B6;
+  font-family: 'Lato', sans-serif;
+  font-size: 12px;
+}
+.title {
+  font-size: 16px;
+  font-family: 'Quicksand', sans-serif;
+  font-weight: bold;
+  color: #253D4E;
+}
+</style>
